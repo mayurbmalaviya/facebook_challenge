05056e830cb3aaa5047f97cdc050e0ab6d6e1e99
mayurbmalaviya <mayurbmalaviya@gmail.com>
2018-08-25 00:06:30 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 include('Facebook/autoload.php');
-$appId = '318140122047320'; //Facebook App ID
-$appSecret = '6231966ffb45d854e27b298bf5bad17f'; // Facebook App Secret
-$homeurl = 'https://mayurbmalaviya.000webhostapp.com/Facebook_App/album.php';  //return to home
+$appId = '{APPID}'; //Facebook_APPID
+$appSecret = '{AppSecretID}'; // Facebook App SecretID
+$homeurl = '{write homeURL}';  //return to home
 $fbPermissions = 'email';  //Required facebook permissions
 $appdefault = 'v3.1';
 $fb = new Facebook\Facebook(array(
